4fbfacbef758e7c2949c1825ac56d50f86f04b3f
Nate Bird <natebird@Nates-MacBook-Pro.local>
2013-02-25 22:55:03 -0500

    Comparison of Rails 3.2.12 -> Rails 4.0.0.beta1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails3app::Application.config.secret_token = '555353eaa014a93b5f3a6240080f1d3438c1f03ecbf7887e043f8200f8c3da634b4ee446218f65b460e010532e48c7e5546f3248a82b6d534316b6d4632f0116'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4app::Application.config.secret_key_base = 'e14c35a8f93ee7262965cc9a8ebe2bc0ea21b02091a791e0bd912783f04b036d2f98d400e4f133115f36d72ecabae71acfe7f3d63b3040890ecfd06359b620a1'
